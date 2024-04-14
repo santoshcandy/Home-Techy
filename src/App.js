25eae9aef33a35e8414aed5aa9c3e2f04c477ff8
santoshcandy <msanthosh11062003@gmail.com>
2024-04-14 16:32:07 +0530

    try

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
 import { BrowserRouter } from 'react-router-dom';
-import LinksCheack from "./MyProject/LinksCheack"
+import LinksCheack from './MyProject/LinksCheack' 
+
 function App() {
   return (
-    <div className="App text-center text-capitalize">
-      <h1>home tech official</h1>
      <BrowserRouter>
         <LinksCheack/>
      </BrowserRouter>
-    </div>
   );
 }
 
