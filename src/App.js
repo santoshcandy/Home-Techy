c930bfd53d5610360c4212c426647105f8ebb4e5
santoshcandy <msanthosh11062003@gmail.com>
2024-04-14 17:14:18 +0530

    hashrouter added instead of browserrouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css"
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import LinksCheack from './MyProject/LinksCheack' 
 
 function App() {
   return (
-     <BrowserRouter>
+    <div className="App text-center text-capitalize">
+
+      <HashRouter>
         <LinksCheack/>
-     </BrowserRouter>
+      </HashRouter>
+    </div>
   );
 }
 
